009764470994524bba4aa6d975e0ca84fbde484b
KrishnaA01 <mahato77@hotmail.com>
2022-01-08 15:25:01 +0100

    added routes & CRUD operations

--- routes/users.js
+++ routes/users.js
@@ -44,8 +44,39 @@ router.delete("/:id", async(req,res)=>{
         return res.status(403).json("You can delete only your account!");
     }
 })
+
+
 //get a user
+router.get("/:id", async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {password,updatedAt,createdAt, ...other} = user._doc
+        res.status(200).json(other)
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 //follow a user
+router.put("/:id/follow", async (req,res)=>{
+    if(req.body.userId !== req.params.id){
+        try{
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.params.userId);
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push:{followers: req.body.userId}})
+                await currentUser.updateOne({$push:{followings: req.body.params.id}})
+                res.status(200).json("user has been followed")
+            }else{
+                res.status(403).json("you already follow this user")
+            }
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(403).json("you can't follow yourself")
+    }
+})
+
 //unfollow user
 
 module.exports = router;
